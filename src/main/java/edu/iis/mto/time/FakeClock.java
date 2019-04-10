c47d6f7615d4839406c6eb4481c8e3e0f12ac593
Dominik <rakdominikrak@gmail.com>
2019-04-10 18:12:35 +0200

    FakeClock Class

---
+++ src/main/java/edu/iis/mto/time/FakeClock.java
@@ -0,0 +1,23 @@
+package edu.iis.mto.time;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.joda.time.DateTime;
+
+public class FakeClock {
+
+    private List<DateTime> datesToReturn;
+
+    public FakeClock() {
+        datesToReturn = new ArrayList<>();
+    }
+
+    public void addDateToReturn(int year, int month, int day, int hour, int minute) {
+        datesToReturn.add(new DateTime(year, month, day, hour, minute));
+    }
+
+    public DateTime getDate() {
+        return datesToReturn.remove(0);
+    }
+}
